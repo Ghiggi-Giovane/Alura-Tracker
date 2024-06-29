404ca036ae4afe30671728c8d0d398c733190a64
Ghiggi-Giovane <ghiggi1234@gmail.com>
2024-06-29 10:58:44 -0300

    atualização

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <!--  Slot quer dizer que o que estiver dentro da TAG BOX onde eu estou importando ele, vai renderizar aqui dentro-->
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MeuBox",
+});
+</script>
+
+<style scoped>
+.box {
+  background: darkgray;
+  color: black;
+}
+</style>
